1f59247a6d965391db5f95efa5764a87c3269580
Harrison Lau <harrison.lau@outlook.com>
2022-07-03 15:36:11 +0800

    update: gitconfig and github setup

--- github-setup.sh
+++ github-setup.sh
@@ -9,7 +9,8 @@ echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed2
 ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
 
-# # If require two github configs
+# # Separate configs for PERSONLA and WORK
+
 # echo "generating a new SSH key for GitHub (personal use)"
 # ssh-keygen -t ed25519 -C "harrison.lau@outlook.com" -f ~/.ssh/id_ed25519_personal
 # eval "$(ssh-agent -s)"
@@ -17,3 +18,10 @@ echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
 # echo "Host personal-github.com\n HostName github.com\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519_personal" | tee ~/.ssh/config
 # ssh-add --apple-use-keychain ~/.ssh/id_ed25519_personal
 # echo "run 'pbcopy < ~/.ssh/id_ed25519_personal.pub' and paste that into GitHub"
+
+# echo "generating a new SSH key for GitHub (work use)"
+# ssh-keygen -t ed25519 -C "<company email>" -f ~/.ssh/id_ed25519_work
+# eval "$(ssh-agent -s)"
+# echo "Host github.com\n HostName github.com\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519_work" >> ~./ssh/config
+# ssh-add --apple-use-keychain ~/.ssh/id_ed25519_work
+# echo "run 'pbcopy < ~/.ssh/id_ed25519_work.pub' and paste that into GitHub"
