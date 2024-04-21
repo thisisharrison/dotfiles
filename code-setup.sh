9b0bac7dca2097ce28dde8213db7ad8944d21569
Harrison Lau <harrison.lau@outlook.com>
2024-04-21 18:24:54 +0800

    update: code-setup

--- code-setup.sh
+++ code-setup.sh
@@ -8,10 +8,9 @@ cat ./Code/extensions.txt | xargs -L 1 code --install-extension
 echo "🤖 Finished installing" 
 
 # Settings, Keybindings, Snippets
-echo "🤖 Symlinking settings, keybindings, snippets..." 
+echo "🤖 Copying settings, keybindings, snippets..." 
 
-ln -s ./Code/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-ln -s ./Code/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s ./Code/snippets/ $HOME/Library/Application\ Support/Code/User
+cat ./Code/keybindings.json >> $HOME/Library/Application\ Support/Code/User/keybindings.json
+cat ./Code/settings.json >> $HOME/Library/Application\ Support/Code/User/settings.json
 
-echo "🤖 Finished symlinking settings, keybindings, snippets..." 
+echo "🤖 Finished copying settings, keybindings, snippets..." 
